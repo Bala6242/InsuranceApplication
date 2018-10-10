02a3da52b6243cebdd3a4dff7a7534e997f5f0b8
Magizh <Magizh@LAPTOP-7H2DF7HI>
2018-10-11 00:30:13 +0530

    Add project files.

---
+++ InsuranceManagement/Areas/Login/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace InsuranceManagement.Areas.Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(InsuranceManagement.Areas.Login.Models.Login objLogin)
+        {
+            if(objLogin.UserName=="Member1" && objLogin.Password=="member@123")
+            {
+                return View("LoginSuccess");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Cridentials";
+                return View("Login");
+            }
+            
+        }
+    }
+}
